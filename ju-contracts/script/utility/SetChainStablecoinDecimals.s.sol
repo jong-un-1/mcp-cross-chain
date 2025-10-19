be9e3e7fa462dfedc31eea2684cd6eeaed9c8e01
jorkerrr <mail@joungun2038.win>
2025-10-19 13:08:58 +0800

    fix: update Solidity version to 0.8.24 and remove unused import in GeniusVault

--- ju-contracts/script/utility/SetChainStablecoinDecimals.s.sol
+++ ju-contracts/script/utility/SetChainStablecoinDecimals.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.8.24;
 
-import {Script, console} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
 
 import {GeniusVault} from "../../src/GeniusVault.sol";
 
--- ju-contracts/src/GeniusVault.sol
+++ ju-contracts/src/GeniusVault.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.24;
 
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {GeniusVaultCore} from "./GeniusVaultCore.sol";
 import {GeniusErrors} from "./libs/GeniusErrors.sol";
 
